2c21e324a5492f21d7506b3b2d07ae64d8253cc4
asdf <secret@kali>
2020-10-18 20:54:00 -0500

    move code to separate files

--- artwork.py
+++ artwork.py
@@ -1,92 +1,15 @@
 #! /usr/bin/env python3
 		
-class Pager:
-	def __init__ (self, results):
-		self.results  = results
-		#self.nquery   = 0
-		#self.page     = {}
-		self.per_page = results.max_page
-		#self.index    = {}
-		#self.session  = session
-	def req (self, **kwargs):
-		# TODO check rate limit ?
-		results  = self.results
-		per_page = self.per_page
-		
-		res  = results.req (1, per_page, **kwargs)
-		vtotal, vtotal_hits, hits, vcreds = res
-		for index in range (0, per_page):
-			res = hits[index]
-			ret = vtotal, vtotal_hits, res, vcreds
-			yield ret
-		
-		max_page = vtotal_hits // per_page
-		for page in range (2, max_page + 1):
-			res  = results.req (page, per_page, **kwargs)
-			vtotal, vtotal_hits, hits, vcreds = res
-			for index in range (0, per_page):
-				res = hits[index]
-				ret = vtotal, vtotal_hits, res, vcreds
-				yield ret
-				
-		page     = max_page + 1
-		max_ndx  = vtotal_hits - (max_page * per_page)
-		if max_ndx != 0:
-			res  = results.req (page, per_page, **kwargs)
-			vtotal, vtotal_hits, hits, vcreds = res
-			for index in range (0, max_ndx):
-				res = hits[index]
-				ret = vtotal, vtotal_hits, res, vcreds
-				yield ret
-				
-		# TODO append vcreds
-		
-	def get_time      (self): return self.results.get_time      ()
-	def get_limit     (self): return self.results.get_limit     ()
-	def get_remaining (self): return self.results.get_remaining ()
-	def get_reset     (self): return self.results.get_reset     ()
-	
-from pixabay_results import PixabayResults
-
-class PixabayPager (Pager):
-	def __init__ (self, session=None, *args, **kwargs):
-		results = PixabayResults (session, *args, **kwargs)
-		Pager.__init__ (self, results, *args, **kwargs)
-	def req (self, **kwargs):
-		rets    = Pager.req (self, **kwargs)
-		for ret in rets:
-			vtotal, vtotal_hits, res, vcreds = ret
-			vcreds = set (vcreds)
-			vcreds.add (res['user'])
-			res    = res['fullHDURL']
-			ret    = vtotal, vtotal_hits, res, vcreds
-			yield ret
-
-from pexels_results import PexelsResults
-
-class PexelsPager (Pager):
-	def __init__ (self, session=None, *args, **kwargs):
-		results =  PexelsResults (session, *args, **kwargs)
-		Pager.__init__ (self, results, *args, **kwargs) 
-	def req (self, **kwargs):
-		rets    = Pager.req (self, **kwargs)
-		for ret in rets:
-			vtotal, vtotal_hits, res, vcreds = ret
-			vcreds = set (vcreds)
-			vcreds.add (res['photographer'])
-			res    = res['url']
-			ret    = vtotal, vtotal_hits, res, vcreds
-			yield ret
-
-
-
-class Buffer:
+class Buffer: # fetch / prefetch results
 	def __init__ (self, pager):
 		self.pager = pager
 	def req (self, **kwargs):
 		pager = self.pager
 		pager.req (**kwargs)
-
+	def get_time      (self): return self.pager.get_time      ()
+	def get_limit     (self): return self.pager.get_limit     ()
+	def get_remaining (self): return self.pager.get_remaining ()
+	def get_reset     (self): return self.pager.get_reset     ()
 
 
 
