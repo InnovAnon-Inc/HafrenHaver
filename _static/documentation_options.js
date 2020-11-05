bf2f305f3b35922fc482608ad9a29e866492d556
lmaddox <InnovAnon-Inc@protonmail.com>
2020-11-05 01:18:50 +0000

    [ci skip] publishing updated documentation...

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'CompletedProcess(args=[&#39;../scripts/version.sh&#39;], returncode=2)',
+    VERSION: 'CompletedProcess(args=[&#39;../scripts/version.sh&#39;], returncode=0)',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
