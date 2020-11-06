847f6e112dc347db58a4a9e3a33d0363fffd1f1b
Innovations Anonymous <InnovAnon-Inc@protonmail.com>
2020-11-05 20:38:33 -0600

    backup

---
+++ tests/test_numerology.py
@@ -0,0 +1,19 @@
+import pytest
+from numerology import ib
+
+
+@pytest.mark.parametrize (
+    "alpha,expected", [('IA', 91), ('FFF', 666), ('HH', 88), ('SS', 1919)]
+)
+def test_a2n1 (alpha, expected): assert a2n1 (alpha) == expected
+
+# a2n0
+# a2b
+# a2bn1
+# a2bn2
+# a2bn11
+# a2bn12
+# a2bn21
+# a2bn22
+# numerology
+
