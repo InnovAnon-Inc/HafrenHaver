adf65dfa679e44a9a88e116b4ed248df07beaf40
Innovations Anonymous <InnovAnon-Inc@protonmail.com>
2020-11-05 22:34:11 -0600

    devops

--- tests/test_numerology.py
+++ tests/test_numerology.py
@@ -4,7 +4,7 @@ from HafrenHaver.numerology import a2n1, a2n0, a2b, a2bn1, a2bn2, a2bn11, a2bn12
 #from numerology import a2n1, a2n0, a2b, a2bn1, a2bn2, a2bn11, a2bn12, a2bn21, a2bn22, numerology
 
 @pytest.mark.parametrize (
-    "alpha,expected", [('IA', 91), ('FFF', 666), ('HH', 88), ('SS', 1919)]
+    "alpha,expected", [('IA', '91'), ('FFF', '666'), ('HH', '88'), ('SS', '1919')]
 )
 def test_a2n1 (alpha, expected): assert a2n1 (alpha) == expected
 
