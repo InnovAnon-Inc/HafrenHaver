8e8ce421d900925312b017a0c88a8e31a6ff0843
Innovations Anonymous <InnovAnon-Inc@protonmail.com>
2020-11-05 21:25:29 -0600

    backup

--- doc/conf.py
+++ doc/conf.py
@@ -24,6 +24,14 @@ from pathlib    import Path
 from os         import path
 from subprocess import run
 
+def read ():
+    vers_name = "VERSION.in"
+    vers_file = path.join (path.dirname (__file__), "..", "scripts", vers_name)
+    vers_file = Path (vers_file)
+    with open (vers_file, 'r') as f: result = f.read ()
+    assert result
+    return result
+
 def get_version ():
     vers_name = "VERSION"
     vers_file = path.join (path.dirname (__file__), "..", vers_name)
@@ -43,6 +51,7 @@ def get_version ():
     if not result: raise Error ()
     return result
 
+version = read ()
 # The full version, including alpha/beta/rc tags
 release = get_version ()
 
@@ -53,15 +62,22 @@ release = get_version ()
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx.ext.todo',
+    'sphinx.ext.githubpages',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
+source_suffix = ['.rst', '.md']
+master_doc = 'index'
+
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+pygments_style = 'sphinx'
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -69,7 +85,10 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+import sphinx_theme
+#html_theme = 'alabaster'
+html_theme = 'stanford_theme'
+html_theme_path = [sphinx_theme.get_html_theme_path ('stanford-theme')]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
