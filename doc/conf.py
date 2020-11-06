acde47e7341b2371982967bac02fdd8a30a9ca19
Innovations Anonymous <InnovAnon-Inc@protonmail.com>
2020-11-06 00:48:46 -0600

    backup

--- doc/conf.py
+++ doc/conf.py
@@ -16,7 +16,8 @@
 
 import os
 import sys
-sys.path.insert (0, os.path.abspath ('../src/HafrenHaver'))
+#sys.path.insert (0, os.path.abspath ('../src/HafrenHaver'))
+sys.path.insert (0, os.path.abspath ('../src'))
 
 # -- Project information -----------------------------------------------------
 
