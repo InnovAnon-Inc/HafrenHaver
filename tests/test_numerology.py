dec1001b1a1bad11d4adb361958d82b5bf830edc
Innovations Anonymous <InnovAnon-Inc@protonmail.com>
2020-11-05 20:43:04 -0600

    backup

--- tests/test_numerology.py
+++ tests/test_numerology.py
@@ -1,6 +1,6 @@
 import pytest
-from numerology import ib
 
+from HafrenHaver.numerology import a2n1, a2n0, a2b, a2bn1, a2bn2, a2bn11, a2bn12, a2bn21, a2bn22, numerology
 
 @pytest.mark.parametrize (
     "alpha,expected", [('IA', 91), ('FFF', 666), ('HH', 88), ('SS', 1919)]
